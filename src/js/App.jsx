88604a283ec8e76887556c4120e94767523f1b7f
Andres Campopiano <acampopiano@gmail.com>
2021-04-29 19:40:35 +0000

    cada vez mas cerca!

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,19 +4,13 @@ import { SecondsCounter } from "./component/secondscounter";
 const App = () => {
 	const [seconds, setSeconds] = useState(0);
 
-	const interval = useRef(null);
-
 	useEffect(() => {
-		if (seconds === 60) stopCounter();
-	}, [seconds]);
+		const interval = setInterval(() => {
+			setSeconds(seconds => seconds + 1);
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
 
-	const startCounter = () =>
-		(interval.current = setInterval(() => {
-			setSeconds(prevState => prevState + 1);
-		}, 1000));
-
-	const stopCounter = () => clearInterval(interval.current);
-	startCounter();
 	return (
 		<div>
 			<SecondsCounter data={seconds} />
