0af20ef9387efbb9d4e7050033051f3152e8dba4
Andres Campopiano <acampopiano@gmail.com>
2021-04-30 12:08:18 +0000

    subo magia

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -2,18 +2,97 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { SecondsCounter } from "./component/secondscounter";
 const App = () => {
-	const [seconds, setSeconds] = useState(0);
+	/*const [seconds, setSeconds] = useState(0);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setSeconds(seconds => seconds + 1);
 		}, 1000);
 		return () => clearInterval(interval);
-	}, []);
+    }, []);*/
+
+	const [second, setSecond] = useState("00");
+	const [minute, setMinute] = useState("00");
+	const [hour, setHour] = useState("00");
+	const [day, setDay] = useState("00");
+	const [month, setMonth] = useState("00");
+	const [year, setYear] = useState("00");
+	const [isActive, setIsActive] = useState(true);
+	const [counter, setCounter] = useState(0);
+
+	function stopTimer() {
+		setIsActive(false);
+		setCounter(0);
+		setSecond("00");
+		setMinute("00");
+		setHour("00");
+		setDay("00");
+		setMonth("00");
+		setYear("00");
+	}
+	useEffect(() => {
+		let intervalId;
+
+		if (isActive) {
+			intervalId = setInterval(() => {
+				const secondCounter = counter % 60;
+				const minuteCounter = Math.floor(counter / 60);
+				const hourCounter = Math.floor(counter / (60 * 60));
+				const dayCounter = Math.floor(counter / (60 * 60 * 24));
+				const monthCounter = Math.floor(counter / (60 * 60 * 24 * 30));
+				const yearCounter = Math.floor(
+					counter / (60 * 60 * 24 * 30 * 12)
+				);
+
+				const computedSecond =
+					String(secondCounter).length === 1
+						? `0${secondCounter}`
+						: secondCounter;
+				const computedMinute =
+					String(minuteCounter).length === 1
+						? `0${minuteCounter}`
+						: minuteCounter;
+				const computedHour =
+					String(hourCounter).length === 1
+						? `0${hourCounter}`
+						: hourCounter;
+				const computedDay =
+					String(dayCounter).length === 1
+						? `0${dayCounter}`
+						: dayCounter;
+				const computedMonth =
+					String(monthCounter).length === 1
+						? `0${monthCounter}`
+						: monthCounter;
+				const computedYear =
+					String(yearCounter).length === 1
+						? `0${yearCounter}`
+						: yearCounter;
+
+				setSecond(computedSecond);
+				setMinute(computedMinute);
+				setHour(computedHour);
+				setDay(computedDay);
+				setMonth(computedMonth);
+				setYear(computedYear);
+
+				setCounter(counter => counter + 1);
+			}, 1000);
+		}
+
+		return () => clearInterval(intervalId);
+	}, [isActive, counter]);
 
 	return (
 		<div>
-			<SecondsCounter data={seconds} />
+			<SecondsCounter
+				second={second}
+				minute={minute}
+				hour={hour}
+				day={day}
+				month={month}
+				year={year}
+			/>
 		</div>
 	);
 };
--- src/js/component/secondscounter.js
+++ src/js/component/secondscounter.js
@@ -1,15 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 
 export const SecondsCounter = props => {
-	const [t6, setT6] = useState(0);
-	const [t5, setT5] = useState(0);
-	const [t4, setT4] = useState(0);
-	const [t3, setT3] = useState(0);
-	const [t2, setT2] = useState(0);
-	const [t1, setT1] = useState(0);
-
 	/*useEffect(() => {
 		//place code here which needs to be run after a state update
 		if (props.data === 60) stopCounter()
@@ -24,22 +17,22 @@ export const SecondsCounter = props => {
 					</div>
 				</div>
 				<div className="t1">
-					<div className="numbers">{t1}</div>
+					<div className="numbers">0</div>
 				</div>
 				<div className="t2">
-					<div className="numbers">{t2}</div>
+					<div className="numbers">0</div>
 				</div>
 				<div className="t3">
-					<div className="numbers">{t3}</div>
+					<div className="numbers">0</div>
 				</div>
 				<div className="t4">
-					<div className="numbers">{t4}</div>
+					<div className="numbers">0</div>
 				</div>
 				<div className="t5">
-					<div className="numbers">{t5}</div>
+					<div className="numbers">{props.minute}</div>
 				</div>
 				<div className="t6">
-					<div className="numbers">{t6}</div>
+					<div className="numbers">{props.second}</div>
 				</div>
 			</div>
 			<h1>Simple Counter with React</h1>
@@ -48,5 +41,6 @@ export const SecondsCounter = props => {
 };
 
 SecondsCounter.propTypes = {
-	data: PropTypes.number
+	second: PropTypes.string,
+	minute: PropTypes.string
 };
